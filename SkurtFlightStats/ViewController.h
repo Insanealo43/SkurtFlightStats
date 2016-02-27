dc5c2738652e1652e978527a125a2da7e0af59c5
Insanealo43 <andrew@letsmend.com>
2016-02-27 12:30:19 -0800

    Initial Commit

---
+++ SkurtFlightStats/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SkurtFlightStats
+//
+//  Created by Andrew Lopez-Vass on 2/27/16.
+//  Copyright © 2016 Andrew Lopez-Vass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SkurtFlightStats/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SkurtFlightStats
+//
+//  Created by Andrew Lopez-Vass on 2/27/16.
+//  Copyright © 2016 Andrew Lopez-Vass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
